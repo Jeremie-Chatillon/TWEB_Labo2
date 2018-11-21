c76b27408beff1b8048118079d5c3201b21844ba
Jeremie-Chatillon <jeremie.chatillon@heig-vd.ch>
2018-11-21 16:17:16 +0100

    as

---
+++ docs/precache-manifest.e1d543bfd108f9506a50cd1143b4395f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3fe92fbe9c13d4d0cfb2",
+    "url": "/static/js/main.3fe92fbe.chunk.js"
+  },
+  {
+    "revision": "bd920eb38be933e58f5b",
+    "url": "/static/js/1.bd920eb3.chunk.js"
+  },
+  {
+    "revision": "3fe92fbe9c13d4d0cfb2",
+    "url": "/static/css/main.199bf1d5.chunk.css"
+  },
+  {
+    "revision": "6f76e6d009b77d338e1077fb02f60c78",
+    "url": "/index.html"
+  }
+];
